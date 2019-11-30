1aa9d58f00caf5a4258de612a9cc8892cea744b8
Taehun Kim <kingmbc@gmail.com>
2019-12-01 08:48:56 +0900

    add logs

--- run_jupyter_docker.sh
+++ run_jupyter_docker.sh
@@ -1,1 +1,3 @@
-jupyter notebook --ip 0.0.0.0 --allow-root
+#jupyter lab --ip 0.0.0.0 --port 9999 --allow-root
+jupyter lab --ip=0.0.0.0 --port=9999 --NotebookApp.token='' --NotebookApp.password='' --allow-root
+
